936fb3c5e39bb3e00ab3d3ddc0b63b7999566493
Aditya Lahiri <96952617+theaditya04@users.noreply.github.com>
2024-10-21 11:53:07 +0530

    Add files via upload

---
+++ Day5/Deep2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.utils import to_categorical
---
+++ Day5/a.py
@@ -0,0 +1,6 @@
+print('hello')
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
